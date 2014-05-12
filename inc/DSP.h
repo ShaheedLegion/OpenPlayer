515b6a31c88d6fb81e3469410b388bbbe14e6c85
Shaheed <shaheed.legion@gmail.com>
2014-05-12 16:29:07 +0200

    Fixed DSP to show correctly
    Fixed the DSP to display the visualization correctly for all frequency
    bands requested. There are random glitches in decoding or in calculating
    the frequency value, which cause the array lookups to overflow
    considerably, so the safety checks in the HandleData function need to
    sty until we have time to figure out what's going wrong with it.

--- inc/DSP.h
+++ inc/DSP.h
@@ -20,30 +20,32 @@ namespace ns_dsp
 class DSP
 {
   public:
-    short * m_data;
-    short * m_buffer;
-    int data_len;
+    //short * m_data;
+    //short * m_buffer;
+    //int data_len;
     IDataHandler * handler;
-    DSP() : m_data(0), m_buffer(0), data_len(100), handler(0)
+    DSP() : /*m_data(0), m_buffer(0), data_len(160),*/ handler(0)
     {
         ns_dsp::g_dsp = this;
-        m_data = new short[data_len];
-        m_buffer = new short[data_len];
+        //m_data = new short[data_len];
+        //m_buffer = new short[data_len];
     }
     ~DSP(){}
 
     static void ProcessAudio(int chan, void *stream, int len, void *udata)
     {
-        int stepsize = (len / ns_dsp::g_dsp->data_len);
-        int idx = 0;
-        for (int i = 0; i < ns_dsp::g_dsp->data_len; i++)
-        {
-            ns_dsp::g_dsp->m_data[i] = ((short *)stream)[idx];
-            idx += stepsize;
-        }
-        memcpy(ns_dsp::g_dsp->m_buffer, ns_dsp::g_dsp->m_data, ns_dsp::g_dsp->data_len);
+        //int stepsize = (len / ns_dsp::g_dsp->data_len);
+        //int idx = 0;
+        //for (int i = 0; i < ns_dsp::g_dsp->data_len; i++)
+        //{
+        //    ns_dsp::g_dsp->m_data[i] = ((short *)stream)[idx];
+        //    idx += stepsize;
+        //}
+        //memcpy(ns_dsp::g_dsp->m_buffer, ns_dsp::g_dsp->m_data, ns_dsp::g_dsp->data_len);
+        //if (ns_dsp::g_dsp->handler)
+        //    ns_dsp::g_dsp->handler->HandleData(ns_dsp::g_dsp->m_buffer, ns_dsp::g_dsp->data_len, chan);
         if (ns_dsp::g_dsp->handler)
-            ns_dsp::g_dsp->handler->HandleData(ns_dsp::g_dsp->m_buffer, ns_dsp::g_dsp->data_len, chan);
+            ns_dsp::g_dsp->handler->HandleData((short*)stream, len, chan);
     }
 };
 #endif // DSP_H_INCLUDED
--- inc/Display.h
+++ inc/Display.h
@@ -7,6 +7,7 @@
 #include <SDL/SDL.h>
 #endif
 #include <limits.h>
+#include <math.h>
 
 #include "EventListener.h"
 #include "DisplayButton.h"
@@ -30,41 +31,30 @@ class Display : public EventListener, public IDataHandler
         //MultiStateButton repeatButton;
         int num_state_buttons;
 
-        //debugging
-        //short l_lowest, r_lowest;
-        //short l_highest, r_highest;
     public:
         Display() : screen(0), visualization(0), num_media_buttons(6), num_state_buttons(3)
         {
             mediaButtons = new DisplayButton[num_media_buttons];
             stateButtons = new ToggleDisplayButton[num_state_buttons];
-
-            //l_lowest = 0;
-            //r_lowest = 0;
-            //l_highest = 0;
-            //r_highest = 0;
         }
 
         ~Display()
-        {
-            // free loaded bitmap
+        {// free loaded bitmap
             SDL_FreeSurface(visualization);
         }
     public:
         void Initialize(SDL_Surface* scr)
         {
             screen = scr;
-            // load an image
-            visualization = SDL_CreateRGBSurface(SDL_SWSURFACE, 100, 100, 32, 0, 0, 0, 0);//SDL_LoadBMP("gui/cb.bmp");
+            visualization = SDL_CreateRGBSurface(SDL_SWSURFACE, 384, 200, 32, 0, 0, 0, 0);
             if (!visualization)
             {
                 printf("Unable to create visualization surface: %s\n", SDL_GetError());
                 return;
             }
 
-            // centre the bitmap on screen
-            dstrect.x = (screen->w - visualization->w) / 2;
-            dstrect.y = 0;//(screen->h - bmp->h) / 2;
+            dstrect.x = 0;
+            dstrect.y = 0;
 
             int bw = 64, bh = 64;
             const char * names[] = {"gui/op_previous.bmp","gui/op_play.bmp","gui/op_pause.bmp","gui/op_stop.bmp","gui/op_next.bmp","gui/op_open.bmp"};
@@ -75,19 +65,15 @@ class Display : public EventListener, public IDataHandler
             const char * states[] = {"gui/op_rep_off.bmp", "gui/op_rep_one.bmp", "gui/op_rep_all.bmp"};
             int statecommands[] = {CMD_REP0, CMD_REP1, CMD_REPA};
             for (int i = 0; i < num_state_buttons; i++)
-                //repeatButton.AddState(states[i], 0, screen->h - (bh * 2), bw, bh);
                 stateButtons[i].Initialize(states[i], (i * bw), screen->h - (bh * 2), bw, bh, statecommands[i]);
         }
 
         int HandleEvent(EventData & data)
         {
-            //printf("Handling display event id[%d] code[%d] x[%d] y[%d]\n", data->ID, data->keycode, data->x, data->y);
             if (data.ID == SDL_MOUSEMOTION)
             {
                 for (int i = 0; i < num_media_buttons; i++)
                     mediaButtons[i].HandleMouseMove(data.x, data.y);
-                //for (int i = 0; i < num_state_buttons; i++)
-                //    stateButtons[i]->HandleMouseMove(data->x, data->y);
             }
             if (data.ID == SDL_MOUSEBUTTONDOWN)
             {
@@ -104,7 +90,6 @@ class Display : public EventListener, public IDataHandler
                         command = stateButtons[i].HandleMouseDown(data.x, data.y);
                         if (command != -1) return command;
                     }
-                    //repeatButton.HandleMouseDown(data.x, data.y);
                 }
             }
             if (data.ID == SDL_MOUSEBUTTONUP)
@@ -113,8 +98,6 @@ class Display : public EventListener, public IDataHandler
                 {
                     for (int i = 0; i < num_media_buttons; i++)
                         mediaButtons[i].HandleMouseUp(data.x, data.y);
-                    //for (int i = 0; i < num_state_buttons; i++)
-                    //    stateButtons[i]->HandleMouseUp(data->x, data->y);
                 }
             }
             return -1;
@@ -131,91 +114,80 @@ class Display : public EventListener, public IDataHandler
                     mediaButtons[i].Render(screen);
                 for (int i = 0; i < num_state_buttons; i++)
                     stateButtons[i].Render(screen);
-                //repeatButton.Render(screen);
             }
 
             SDL_Flip(screen); // finally, update the screen :)
         }
         void HandleData(short * buff, int len, int chan)
         {
-            //short l_lowest = 0, r_lowest = 0;
-            //short l_highest = 0, r_highest = 0;
-/*
-            short l;
-            short r;
-            for (int i = 0; i < len; i+=2)
-            {
-                l = buff[i];
-                r = buff[i + 1];
-
-                if (l < l_lowest) l_lowest = l;
-                if (r < r_lowest) r_lowest = r;
-
-                if (r > r_highest) r_highest = r;
-                if (l > l_highest) l_highest = l;
-            }
-*/
-            //now copy the data to the screen using some algorithm...
-
+            //printf("HandleData - stream channel[%d] length [%d]\n", chan, len);
             //printf("HandleData called channels[%d] lowest l[%d] r[%d] highest l[%d] r[%d]\n", chan, l_lowest, r_lowest, l_highest, r_highest);
 
             if (visualization)
             {   //possibly clear out viz here
-                SDL_FillRect(visualization, 0, SDL_MapRGB(visualization->format, 0, 0, 0));
+                SDL_FillRect(visualization, 0, SDL_MapRGB(visualization->format, 255, 64, 64));
                 //perform some crazy math on viz...
                 SDL_LockSurface(visualization);
-                //we will assume that len is the same as viz width, but do some math here just in case.
-                int indices = len;//(len < visualization->w ? len : visualization->w);
+                int indices = (len / 160);
                 int * pixels = (int*)visualization->pixels;
 
                 if (pixels)
                 {
-                    Uint32 c_white = SDL_MapRGB(visualization->format, 255,255,255);
-                    //int combinedvalue;
-                    short lval;
-                    double ldval;
-                    //short rval;
+                    Uint32 c_white = SDL_MapRGB(visualization->format, 255, 255, 255);
+                    short lval, rval;
+                    double ldval, rdval;
                     int offset = visualization->h / 2;
                     int l_idx;
                     int r_idx;
-                    for (int i = 0; i < indices; i+=2)
+
+                    int idxoffset = 0;
+                    for (int i = 0; i < indices; i++)
                     {
-                        lval = buff[i];//(32000 / buff[i]) * 50;
-                        //rval = buff[i + 1];//(32000 / buff[i + 1]) * 50;
+                        lval = buff[idxoffset];
+                        rval = buff[idxoffset + 1];
+
+                        idxoffset += 80;
 
                         if (lval)
                         {
-                            if (lval < 0) ldval = (double)((double)lval / (-32768));
-                            if (lval > 0) ldval = (double)((double)lval / (32768));
+                            if (lval < 0) ldval = (double)((double)lval / (SHRT_MIN));
+                            if (lval > 0) ldval = (double)((double)lval / (SHRT_MAX));
+                            if (rval < 0) rdval = (double)((double)rval / (SHRT_MIN));
+                            if (rval > 0) rdval = (double)((double)rval / (SHRT_MAX));
 
                             //printf("Clamped lval[%f]\n", ldval);
 
                             ldval *= 50;
+                            rdval *= 50;
 
-                            if (ldval > 50.0)
-                                printf("Exceeded! Transfored lval [%f]\n", ldval);
+                            if (ldval > 50.0 || rdval > 50.0 || ldval < -50.0 || rdval < -50.0)
+                                printf("Exceeded! Transformed lval [%f] rval[%f]\n", ldval, rdval);
                         }
-                        //lval = (32000 / (lval + 255)) * 50;
-                        //rval = (32000 / (rval + 255)) * 50;
-                        //printf("Initial data values l[%d] r[%d]\n", lval, rval);
-                        //if (lval > 50)lval = 50;
-                        //if (rval > 50)rval = 50;
-                        //if (lval < 0)lval = 0;
-                        //if (rval < 0)rval = 0;
-                        //combinedvalue = (int)((int)lval << 16 | rval);
 
-                        l_idx = i + (offset - (int)(ldval)) * visualization->w;
-                        r_idx = i + (offset + (int)(ldval)) * visualization->w;
-                        //printf("Transformed data values l[%d] r[%d]\n", lval, rval);
-                        //printf("Setting indices i[%d] l[%d] r[%d]\n", i, l_idx, r_idx);
-                        pixels[l_idx] = c_white;
-                        pixels[r_idx] = c_white;
-                        //Draw_VLine(visualization, i, offset - lval, offset + rval, c_white);
+                        int index = i * 24;
+                        for (int j = 0; j < 24; j++)
+                        {
+                            l_idx = index + ((offset - floor(ldval)) * visualization->w);
+                            r_idx = index + ((offset + floor(rdval)) * visualization->w);
+                            if (l_idx > (visualization->w * visualization->h) || l_idx < 0)
+                            {
+                                printf("left index exceeded bounds! idx[%d] bounds[%d]\n", l_idx, (visualization->w * visualization->h));
+                                continue;
+                            }
+                            if (r_idx > (visualization->w * visualization->h) || r_idx < 0)
+                            {
+                                printf("right index exceeded bounds! idx[%d] bounds[%d]\n", r_idx, (visualization->w * visualization->h));
+                                continue;
+                            }
+                            //printf("Setting indices i[%d] l[%d] r[%d]\n", i, l_idx, r_idx);
+                            pixels[l_idx] = c_white;
+                            pixels[r_idx] = c_white;
+                            index++;
+                        }
                     }
                 }
                 SDL_UnlockSurface(visualization);
             }
-
         }
 };
 
